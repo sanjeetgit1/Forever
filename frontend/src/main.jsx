5b9e4f90f0ebfd536fe22b1ac28b967785dc77e5
sanjeetgit1 <sksanjeet616@gmail.com>
2024-12-23 10:10:59 +0530

    mainjs changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,9 +6,10 @@ import { BrowserRouter } from 'react-router-dom'
 import ShopContextProvider from './context/ShopContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/">
   <ShopContextProvider>
     <App />
   </ShopContextProvider>
   </BrowserRouter>,
 )
+
